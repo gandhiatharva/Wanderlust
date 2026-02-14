36ccec77b090f9a5023a76ce8f7edee38c2d568f
Atharva Gandhi <atharvagandhi2005@gmail.com>
2026-02-14 17:11:35 +0530

    Fix currUser undefined crash on Render

--- app.js
+++ app.js
@@ -230,10 +230,17 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
+//     res.locals.success = req.flash("success");
+//     res.locals.error = req.flash("error");
+//     res.locals.currUser = req.user;
+//     next();
+// });
+
+app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;  
     next();
 });
 
